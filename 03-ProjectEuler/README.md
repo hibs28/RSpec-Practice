7287e90d6d919de1fcd3e76e01e67ed579c7f9b0
Tech-A06 <hibah28@live.co.uk>
2019-01-24 17:37:27 +0000

    completed problem1 and problem2 in Project Euler

---
+++ 03-ProjectEuler/README.md
@@ -0,0 +1,5 @@
+# Project Euler TDD
+
+Problem One - https://projecteuler.net/problem=1
+
+Problem Two - https://projecteuler.net/problem=2
---
+++ 03-ProjectEuler/lib/problem.rb
@@ -0,0 +1,32 @@
+class Problem
+  def initialize
+    
+  end
+
+  def problem1(number)
+    sum =0
+    1.upto(number-1) do |i|
+      if i%3 == 0 || i%5 ==0
+        sum=sum+i
+      end
+    end  
+    sum
+  end
+
+  def problem2(number)
+    counter=0
+    num1 = 2
+    num2 = 1
+    until counter >= number
+      if num1.even? 
+        counter = counter + num1
+      end
+      sum = num1 + num2 
+      num2 = num1
+      num1 = sum
+    end
+    counter
+  end
+
+
+end
